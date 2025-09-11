154293560530e7dcc4746f084f559a8415e70d02
David Kornel <kornys@outlook.com>
2025-09-11 15:20:50 +0200

    Update copy-script script to copy generated allure test reports
    Signed-off-by: David Kornel <kornys@outlook.com>

--- tmt/scripts/copy-logs.sh
+++ tmt/scripts/copy-logs.sh
@@ -2,14 +2,18 @@
 
 
 TEST_LOG_DIR="${TMT_PLAN_DATA}/../discover/default-0/tests/streams-e2e/target/logs"
-XUNIT_LOG_DIR="${TMT_PLAN_DATA}/../discover/default-0/tests/target/streams-e2e/failsafe-reports"
+XUNIT_LOG_DIR="${TMT_PLAN_DATA}/../discover/default-0/tests/streams-e2e/target/failsafe-reports"
+REPORT_LOG_DIR="${TMT_PLAN_DATA}/../discover/default-0/tests/streams-e2e/target/site"
 
 TARGET_DIR="${TMT_PLAN_DATA}"
 LOGS_DIR="${TARGET_DIR}/logs"
 XUNIT_DIR="${TARGET_DIR}/xunit"
+REPORT_DIR="${TARGET_DIR}/report"
 
 mkdir -p "${LOGS_DIR}"
 mkdir -p "${XUNIT_DIR}"
+mkdir -p "${REPORT_DIR}"
 
 cp -R "${TEST_LOG_DIR}" "${LOGS_DIR}" || true
 cp -R "${XUNIT_LOG_DIR}" "${XUNIT_DIR}" || true
+cp -R "${REPORT_LOG_DIR}" "${REPORT_DIR}" || true
